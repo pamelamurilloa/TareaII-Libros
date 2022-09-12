9126aca26b294197111bcb556ef283fe0f1bae24
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-12 11:00:15 -0600

    The administrator is now added to the files when it starts

--- TareaII_Libros/src/MainStartFolder/MainStart.java
+++ TareaII_Libros/src/MainStartFolder/MainStart.java
@@ -1,12 +1,22 @@
 package MainStartFolder;
 
 import InterfacesFolder.LoginFrame;
+import LogicFolder.ArchiveManager;
 
 public class MainStart {
 
     public static void main(String[] args) {
+        addAdministrator();
+        
         LoginFrame mainFrame = new LoginFrame();
         mainFrame.setVisible(true);
     }
+    
+    public static void addAdministrator() {
+        ArchiveManager archiveManager = new ArchiveManager();
+        archiveManager.createFileUsers();
+        archiveManager.writeInFile("users", "01Admin, administrador, password"); //The 1st is the file (users/books), the 2nd is the information that will be written
+        
+    }    
     
 }
