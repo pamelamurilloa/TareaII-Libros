566e63b1a8d1f8a6b9e4fc80c046a84350c7d788
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-12 18:34:05 -0600

    New class to do everything related to moving books from and to the archive

---
+++ TareaII_Libros/src/LogicFolder/BookModifier.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LogicFolder;
+
+/**
+ *
+ * @author pamelamurillo
+ */
+public class BookModifier {
+    //This will delete and add books in the file
+    //book counter too
+}
