f23b67cce04645bc5e5b12e0e68aff911c50960f
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-12 21:57:14 -0600

    Two new methods in BookModifier, one to add a book and the other one to get the complete list as a hashmap

--- TareaII_Libros/src/LogicFolder/BookModifier.java
+++ TareaII_Libros/src/LogicFolder/BookModifier.java
@@ -1,14 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package LogicFolder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
  * @author pamelamurillo
  */
 public class BookModifier {
-    //This will delete and add books in the file
-    //book counter too
+    
+    
+    public void addBook(String[] bookData) { //bookData[0] = bookName, bookData[1] = timesBought, bookData[2] = timesRented
+        String newLine = bookData[0] + ", " + bookData[1] + ", " + bookData[2];
+        
+        ArchiveManager archiveManager = new ArchiveManager();
+        archiveManager.createFileBooks();
+        archiveManager.writeInFile("books", newLine);
+        
+    }
+    
+    public HashMap<String, HashMap> getBookList() {
+        HashMap<String, HashMap> bookHashMap = new HashMap<>();
+        
+        ArchiveManager archiveManager = new ArchiveManager();
+        archiveManager.createFileBooks();
+        ArrayList bookList = archiveManager.readInFile("books");
+        
+        for (int line = 0; bookList.size() > line; line++) {
+            String[] bookData = bookList.get(line).toString().split(", ");
+            HashMap<String, String> subBook = new HashMap<>();
+            
+            subBook.put( "timesBought", bookData[1] );
+            subBook.put( "timesRented", bookData[2] );
+            
+            bookHashMap.put(bookData[0], subBook);
+        }
+        
+        return bookHashMap;
+        
+    }
+    
+    
+    public void deleteBook(String bookName){
+        
+    }
+    
+    
 }
